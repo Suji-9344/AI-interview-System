fa1cc163bf704fa5bb26eabed54e4faa0fa37cb7
Suji-9344 <sujitha9344@gmail.com>
2026-02-08 12:20:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title("🎤 AI Interview Evaluation System")
 col1, col2 = st.columns([1,3])
 
 with col1:
-    st.image("hr_avatar.png", width=150)
+    st.image("hr_avatar.jpeg", width=150)
 
 with col2:
     st.markdown("### 🤖 AI HR Interviewer")
